6560b4c72c950b08c40c21ec550b0c2dcdeb2c0f
Patrick Quilty <pquilty79@gmail.com>
2022-03-24 09:49:01 -0500

    added falsh to xylophone button

--- Xylophone/Xylophone/ContentView.swift
+++ Xylophone/Xylophone/ContentView.swift
@@ -20,70 +20,115 @@ struct ContentView: View {
                 }, label: {
                     Text("C")
                         .frame(width: 380, height: 95, alignment: .center)
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
             })
                 .background(.blue)
                 .cornerRadius(10)
+                .buttonStyle(MyPrimitiveButtonStyle())
+                
+                
                 Button(action:{
                     Sounds.playSounds(soundfile: "note2.wav")
                 }, label: {
                     Text("D")
                         .frame(width: 365, height: 95, alignment: .center)
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
             })
                 .background(.red)
                 .cornerRadius(10)
+                .buttonStyle(MyPrimitiveButtonStyle())
+                
                 Button(action:{
                     Sounds.playSounds(soundfile: "note3.wav")
                 }, label: {
                     Text("E")
                         .frame(width: 350, height: 95, alignment: .center)
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
             })
                 .background(.yellow)
                 .cornerRadius(10)
+                .buttonStyle(MyPrimitiveButtonStyle())
+                
                 Button(action:{
                     Sounds.playSounds(soundfile: "note4.wav")
                 }, label: {
                     Text("F")
                         .frame(width: 335, height: 95, alignment: .center)
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
             })
                 .background(.purple)
                 .cornerRadius(10)
+                .buttonStyle(MyPrimitiveButtonStyle())
+                
                 Button(action:{
                     Sounds.playSounds(soundfile: "note5.wav")
                 }, label: {
                     Text("G")
                         .frame(width: 330, height: 95, alignment: .center)
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
             })
                 .background(.orange)
                 .cornerRadius(10)
+                .buttonStyle(MyPrimitiveButtonStyle())
+                
                 Button(action:{
                     Sounds.playSounds(soundfile: "note6.wav")
                 }, label: {
                     Text("A")
                         .frame(width: 315, height: 95, alignment: .center)
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
             })
                 .background(.pink)
                 .cornerRadius(10)
+                .buttonStyle(MyPrimitiveButtonStyle())
+                
                 Button(action:{
                     Sounds.playSounds(soundfile: "note7.wav")
                 }, label: {
                     Text("B")
                         .frame(width: 300, height: 95, alignment: .center)
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
             })
                 .background(.green)
                 .cornerRadius(10)
+                .buttonStyle(MyPrimitiveButtonStyle())
             }
         }
         
 
     }
 
+}
+
+struct MyPrimitiveButtonStyle: PrimitiveButtonStyle {
+
+    func makeBody(configuration: PrimitiveButtonStyle.Configuration) -> some View {
+        MyButton(configuration: configuration)
+    }
+
+    struct MyButton: View {
+        @GestureState private var pressed = false
+
+        let configuration: PrimitiveButtonStyle.Configuration
+//        let color: Color = .green
+
+        @State private var didTriggered = false
+
+        var body: some View {
+            // you can set minimumDuration to Double.greatestFiniteMagnitude if you think that
+            // user can hold button for such a long time
+            let longPress = LongPressGesture(minimumDuration: 300, maximumDistance: 300.0)
+                .updating($pressed) { value, state, _ in
+                    state = value
+                    self.configuration.trigger()
+            }
+
+            return configuration.label
+                .background(.white)
+                .opacity(pressed ? 0.0 : 0.4)
+                .gesture(longPress)
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
