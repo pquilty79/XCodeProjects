be9e6ff3cf3395226bb8dbbe0d942bc22c28848f
Patrick Quilty <pquilty79@gmail.com>
2022-06-22 11:19:34 -0500

    finished state management

---
+++ scrumdinger/scrumdinger/Models/AVPlayer+Ding.swift
@@ -0,0 +1,16 @@
+//
+//  AVPlayer+Ding.swift
+//  scrumdinger
+//
+//  Created by Patrick Quilty on 6/22/22.
+//
+
+import Foundation
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
