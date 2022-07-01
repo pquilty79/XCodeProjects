6f5a983e92d72812a230d02c2c17258001cea243
Patrick Quilty <pquilty79@gmail.com>
2022-07-01 10:28:31 -0500

    added mail feature to iexpense

--- iExpense/iExpense/ExpenseView.swift
+++ iExpense/iExpense/ExpenseView.swift
@@ -6,14 +6,41 @@
 //
 
 import SwiftUI
+import MessageUI
 
 struct ExpenseView: View {
     let expense: ExpenseItem
+    @State var result: Result<MFMailComposeResult, Error>? = nil
+    @State var isShowingMailView = false
 
     var body: some View {
-        Image(uiImage: expense.receipt.getImage() ?? #imageLiteral(resourceName: "CodeUpCertificate.png"))
-            .resizable()
-            .scaledToFit()
+        NavigationView {
+            VStack {
+                HStack {
+                VStack {
+                    Text("\(expense.name) - \(expense.location)").font(.headline)
+                    Text(expense.type)
+                }
+                Spacer()
+                Text(expense.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    }
+                Image(uiImage: expense.receipt.getImage() ?? #imageLiteral(resourceName: "CodeUpCertificate.png"))
+                    .resizable()
+                .scaledToFit()
+                Spacer()
+            }
+        }
+        .toolbar {
+            Button {
+                self.isShowingMailView.toggle()
+            } label: {
+                Image(systemName: "mail")
+            }
+            .disabled(!MFMailComposeViewController.canSendMail())
+            .sheet(isPresented: $isShowingMailView) {
+                MailView(result: self.$result)
+            }
+        }
     }
 }
 
