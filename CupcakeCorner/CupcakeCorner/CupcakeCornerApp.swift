d275a31f5fb8eb87549970e92edcb30b26bd2dae
Patrick Quilty <pquilty79@gmail.com>
2022-07-07 19:56:21 -0500

    starting cupcake corner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Patrick Quilty on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
